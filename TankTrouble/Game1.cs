fc4e648276b638923bcd849740bb741c56c72b4b
wlyons04 <wrl5787@rit.edu>
2022-10-21 19:13:26 -0400

    copying andrew

--- TankTrouble/Game1.cs
+++ TankTrouble/Game1.cs
@@ -35,7 +35,6 @@ namespace TankTrouble
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             System.Console.WriteLine("Will smells");
-            System.Console.WriteLine("nah thats cap big g");
             // TODO: Add your update logic here
 
             base.Update(gameTime);
